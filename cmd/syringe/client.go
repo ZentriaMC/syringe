8b82644db868e710cb93dc5044f6f88c356ca158
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-05-05 10:21:12 +0300

    Extract requestCredential to separate file

---
+++ cmd/syringe/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"runtime"
+
+	"github.com/ZentriaMC/syringe/internal/request"
+	"github.com/ZentriaMC/syringe/internal/secret"
+)
+
+func requestCredential(socket string, cr request.CredentialRequest, out io.Writer) (n int64, err error) {
+	var conn *net.UnixConn
+	var laddr, raddr *net.UnixAddr
+	var request []byte
+
+	if raddr, err = net.ResolveUnixAddr("unix", socket); err != nil {
+		return
+	}
+
+	if request, err = cr.Bytes(); err != nil {
+		err = fmt.Errorf("failed to serialize the credential request: %w", err)
+		return
+	}
+
+	if runtime.GOOS == "linux" {
+		laddr = &net.UnixAddr{
+			Net:  raddr.Net,
+			Name: string(request),
+		}
+	}
+
+	if conn, err = net.DialUnix("unix", laddr, raddr); err != nil {
+		err = fmt.Errorf("failed to connect to '%s': %w", socket, err)
+		return
+	}
+
+	defer func() { _ = conn.Close() }()
+
+	if n, err = io.Copy(out, io.LimitReader(conn, secret.MAX_CREDENTIAL_SIZE)); err != nil {
+		err = fmt.Errorf("failed to read credential request response: %w", err)
+		return
+	}
+	return
+}
--- cmd/syringe/update_linux.go
+++ cmd/syringe/update_linux.go
@@ -179,36 +179,3 @@ func createReplacementSecret(credsDir, credential string, f func(io.Writer) erro
 
 	return
 }
-
-func requestCredential(socket string, cr request.CredentialRequest, out io.Writer) (n int64, err error) {
-	var conn *net.UnixConn
-	var laddr, raddr *net.UnixAddr
-	var request []byte
-
-	if raddr, err = net.ResolveUnixAddr("unix", socket); err != nil {
-		return
-	}
-
-	if request, err = cr.Bytes(); err != nil {
-		err = fmt.Errorf("failed to serialize the credential request: %w", err)
-		return
-	}
-
-	laddr = &net.UnixAddr{
-		Net:  raddr.Net,
-		Name: string(request),
-	}
-
-	if conn, err = net.DialUnix("unix", laddr, raddr); err != nil {
-		err = fmt.Errorf("failed to connect to '%s': %w", socket, err)
-		return
-	}
-
-	defer func() { _ = conn.Close() }()
-
-	if n, err = io.Copy(out, io.LimitReader(conn, secret.MAX_CREDENTIAL_SIZE)); err != nil {
-		err = fmt.Errorf("failed to read credential request response: %w", err)
-		return
-	}
-	return
-}
